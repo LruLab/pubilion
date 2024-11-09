ce709b943715c8c3d775dfa686885a4a7b9cc632
KL-Lru <26233827+KL-Lru@users.noreply.github.com>
2024-11-10 01:12:31 +0900

    to

--- lib/pubilion.rb
+++ lib/pubilion.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-require_relative "pubilion/version"
-require_relative "pubilion/config"
-require_relative "pubilion/errors"
-require_relative "pubilion/extensions/active_job/queue_adapter"
+require "pubilion/version"
+require "pubilion/errors"
+require "pubilion/config"
+
+require "pubilion/extensions"
 
 # Pubilion is a Ruby library for interacting with Google Cloud Pub/Sub.
 module Pubilion
---
+++ lib/pubilion/extensions.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+require "pubilion/extensions/active_job/queue_adapter"
